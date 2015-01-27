cea949140337e14be1c9d009e3776b9a12764531
Konstantinos Drossos <kostas.drosos@icloud.com>
2015-01-27 17:34:11 +0200

    Fixed xml

--- Assets/Scripts/SoundGraphicsEngine/AudioFilesSettings.cs
+++ Assets/Scripts/SoundGraphicsEngine/AudioFilesSettings.cs
@@ -125,6 +125,9 @@ public class AudioFilesSettings {
 	 * @author Konstantinos Drossos
 	 */
 	public string getSoundForPlayer (int player, string theCase, UnityEngine.Vector3 soundOrigin) {
+		string tmp = this.audioFilesForPlayer [player].Find ( delegate (AudioFileForGame af) {
+			if ( af.TheCase.Equals (theCase) && af.ThePosition.Equals (soundOrigin) ) return true; else return false; 
+		}).ThePath;
 		return this.audioFilesForPlayer [player].Find ( delegate (AudioFileForGame af) {
 			if ( af.TheCase.Equals (theCase) && af.ThePosition.Equals (soundOrigin) ) return true; else return false; 
 		}).ThePath;
