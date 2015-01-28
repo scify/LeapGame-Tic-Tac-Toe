285c3f7c9ce757aaafeb0607d6db45ce056382ca
Konstantinos Drossos <kostas.drosos@icloud.com>
2015-01-28 09:51:33 +0200

    Added comments in the classes inside GameEngine directory

--- Assets/Scripts/GameEngine/Actor.cs
+++ Assets/Scripts/GameEngine/Actor.cs
@@ -1,3 +1,16 @@
+/**
+ * The file for the Actor class. 
+ * 
+ * This file holds the definition and implementation 
+ * of Actor class. This file is part of
+ * the LEAP project. 
+ * 
+ * @file Actor.cs
+ * @version 1.0
+ * @date 14/01/2015 (dd/mm/yyyy)
+ * @author Nick Zorbas
+ * @copyright ??? distributed as is under MIT Licence.
+ */
 using UnityEngine;
 using System.Collections;
 
--- Assets/Scripts/GameEngine/GameEvent.cs
+++ Assets/Scripts/GameEngine/GameEvent.cs
@@ -1,3 +1,16 @@
+/**
+ * The file for the GameEvent class. 
+ * 
+ * This file holds the definition and implementation 
+ * of GameEvent class. This file is part of
+ * the LEAP project. 
+ * 
+ * @file GameEvent.cs
+ * @version 1.0
+ * @date 14/01/2015 (dd/mm/yyyy)
+ * @author Nick Zorbas
+ * @copyright ??? distributed as is under MIT Licence.
+ */
 using UnityEngine;
 using System.Collections;
 
